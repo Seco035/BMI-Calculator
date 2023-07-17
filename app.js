23733b6a78d0d331666a9f91cba3adf8695ad05f
Selcukibar <selcukibarr@icloud.com>
2023-07-17 18:19:10 +0300

    range values are fixed

--- app.js
+++ app.js
@@ -1,24 +1,37 @@
 function calculate(){
-    let height = +(document.querySelector("#heightInput").value)
+    let height = +(document.querySelector("#heightInput").value),
     
-    let weight = +(document.querySelector("#weightInput").value)
+    weight = +(document.querySelector("#weightInput").value),
     
-    let bmi = weight / ((height / 100) ** 2)
+    bmi = weight / ((height / 100) ** 2),
 
-    let result = document.querySelector("#result")
+    result = document.querySelector("#result")
     
     if(bmi < 18.5){
-        result.innerHTML = `Your BMI : ${bmi.toFixed(2)} \n You have an underweight body weight. Be careful！ `
+        result.innerHTML = `Your BMI : ${bmi.toFixed(2)} \n\n You have an underweight body weight. Be careful！ `
     }
     else if (bmi < 25){
-        result.innerHTML = `Your BMI : ${bmi.toFixed(2)} \n You have a normal body weight. Good job!`
+        result.innerHTML = `Your BMI : ${bmi.toFixed(2)} \n\n You have a normal body weight. Good job!`
     }
     else if (bmi < 30){
-        result.innerHTML = `Your BMI : ${bmi.toFixed(2)} \n You are overweight. It is advisable to focus on a balanced diet and regular exercise.`
+        result.innerHTML = `Your BMI : ${bmi.toFixed(2)} \n\n You are overweight. It is advisable to focus on a balanced diet and regular exercise.`
     }
     else {
-        result.innerHTML = `Your BMI : ${bmi.toFixed(2)} You are obese. Consult with a healthcare professional for personalized advice and support.`
+        result.innerHTML = `Your BMI : ${bmi.toFixed(2)} \n\n You are obese. Consult with a healthcare professional for personalized advice and support.`
     }
-    
      
 }
+
+let weight = document.querySelector("#weightInput"),
+weightSpan = document.querySelector("#weightSpan")
+
+weight.addEventListener("input", () => {
+    weightSpan.textContent = weight.value
+})
+
+let input2 = document.querySelector("#ageInput"),
+span2 = document.querySelector("#ageSpan")
+
+input2.addEventListener("input", () => {
+    span2.textContent = input2.value
+})
